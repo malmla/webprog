726dbffa8ab52df9a62d51ebd294d90bed719ad1
martin <martin@mandella.se>
2023-09-21 19:31:54 +0200

    fan skit gegleehs

--- labs/lab2/src/ViewOrder.js
+++ labs/lab2/src/ViewOrder.js
@@ -1,22 +1,21 @@
 
 function ViewOrder (props) {
-  const orders = Object.assign(props.saladOrders);
-    return (
-    <div className="container col-12">
-      <div className="row h-200 p-5 bg-light border rounded-3">
-        <h2>Varukorg</h2>
-        Order-id: {orders.uuidOrder}
-        <ul>
-          {console.log(orders['saladList'])}
-          {
-            Object.entries(orders.saladList).map(salad => 
-              <li key={salad[0]}>{`${Object.keys(salad[1]['ingredients'])} ${salad[1].getPrice()} kr`}</li>
-            )
-          }                            
-        </ul>
+  //const orders = Object.assign(props.saladOrders);
+  return (
+  <div className="container col-12">
+    <div className="row h-200 p-5 bg-light border rounded-3">
+      <h2>Varukorg</h2>
+      Order-id: {props.saladOrders.uuidOrder}
+      <ul>
+        {
+          Object.entries(props.saladOrders.saladList).map(salad => 
+            <li key={salad[0]}>{`${Object.keys(salad[1]['ingredients']).map(ing => ` ${ing}`)}: ${salad[1].getPrice()} kr`}</li>
+          )
+        }                            
+      </ul>
 
-      </div>
     </div>
-    );
+  </div>
+  );
 }
 export default ViewOrder;
